250c84837f756c4b82245922959005dc610124f6
Akshattuknait <100125741+Akshattuknait@users.noreply.github.com>
2022-03-02 13:24:11 +0530

    Update TestController.java

--- src/main/java/com/bezkoder/springjwt/controllers/TestController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/TestController.java
@@ -11,23 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController
 {
+  
   @GetMapping("/all")
   public String allAccess() {
     Role role = new Role();
     role.setId(1);
-    role.setName(ERole.valueOf(String.valueOf(ROLE_USER)));
+    role.setName(ERole.ROLE_USER);
     roleRepository.save(role);
     Role role2 = new Role();
     role.setId(2);
-    role.setName(ERole.valueOf(String.valueOf(ROLE_MODERATOR)));
+    role.setName(ERole.ROLE_MODERATOR);
     roleRepository.save(role);
     Role role3 = new Role();
     role.setId(3);
-    role.setName(ERole.valueOf(String.valueOf(ROLE_ADMIN)));
+    role.setName(ERole.ROLE_ADMIN);
     roleRepository.save(role);
     return "Public Content.";
   }
-
 
   @GetMapping("/user")
   @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
