2bc19705de87aab2dd010e900bf28f5b6760d52f
akshat <tuknaitakshat07@gmail.com>
2022-03-15 13:58:56 +0530

    New Functions Added

--- src/main/java/com/bezkoder/springjwt/payload/request/TokenRefreshRequest.java
+++ src/main/java/com/bezkoder/springjwt/payload/request/TokenRefreshRequest.java
@@ -2,7 +2,8 @@ package com.bezkoder.springjwt.payload.request;
 
 import javax.validation.constraints.NotBlank;
 
-public class TokenRefreshRequest {
+public class TokenRefreshRequest
+{
     @NotBlank
     private String refreshToken;
     public String getRefreshToken() {
